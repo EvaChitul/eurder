3280b43434b1891610fc13b719b833b4c7cc363b
Eva Chitul <eva.chitul@coleurope.eu>
2021-03-30 11:12:11 +0200

    Added application file, logger and updated properties

---
+++ war/src/main/java/com/switchfully/eurder/EurderApplication.java
@@ -0,0 +1,11 @@
+package com.switchfully.eurder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurderApplication.class, args);
+    }
+}
---
+++ war/src/test/java/com/switchfully/eurder/EurderApplicationTest.java
@@ -0,0 +1,14 @@
+package com.switchfully.eurder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EurderApplicationTest {
+
+    @Test
+    void ContextTest(){
+
+    }
+
+}
